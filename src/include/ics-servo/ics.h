46cf1f8def6bdd75143b944bd571efb04cfa7866
coord.e <me@coord-e.com>
2018-10-17 00:27:20 +0900

    Change: Use Per-servo class

--- src/include/ics-servo/ics.h
+++ src/include/ics-servo/ics.h
@@ -1,19 +1,20 @@
+class Servo {
+public:
+  Servo(ServoID);
 
-class IcsServo {
-  using ServoID = std::uint8_t;
   using Position = double;
 
-  void set_position(ServoID, Position);
-  void set_free(ServoID);
+  void set_position(Position);
+  void set_free();
 
-  void set_stretch(ServoID, std::uint8_t stretch);
-  void set_speed(ServoID, std::uint8_t speed);
-  void set_current_limit(ServoID, std::uint8_t current_limit);
-  void set_temperature_limit(ServoID, std::uint8_t tmperature_limit);
+  void set_stretch(std::uint8_t stretch);
+  void set_speed(std::uint8_t speed);
+  void set_current_limit(std::uint8_t current_limit);
+  void set_temperature_limit(std::uint8_t tmperature_limit);
 
-  std::uint8_t get_stretch(ServoID id);
-  std::uint8_t get_speed(ServoID id);
-  std::uint8_t get_current(ServoID id);
-  std::uint8_t get_temperature(ServoID id);
-  Position get_position(ServoID id);
+  std::uint8_t get_stretch();
+  std::uint8_t get_speed();
+  std::uint8_t get_current();
+  std::uint8_t get_temperature();
+  Position get_position();
 };
