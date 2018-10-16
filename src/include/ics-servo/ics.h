94580b4fc2fb784e915dd318b7ddf21709a29d9f
coord.e <me@coord-e.com>
2018-10-17 00:25:37 +0900

    Fix: setter functions don't return

--- src/include/ics-servo/ics.h
+++ src/include/ics-servo/ics.h
@@ -3,13 +3,13 @@ class IcsServo {
   using ServoID = std::uint8_t;
   using Position = double;
 
-  int set_position(ServoID, Position);
-  int set_free(ServoID);
+  void set_position(ServoID, Position);
+  void set_free(ServoID);
 
-  int set_stretch(ServoID, std::uint8_t stretch);
-  int set_speed(ServoID, std::uint8_t speed);
-  int set_current_limit(ServoID, std::uint8_t current_limit);
-  int set_temperature_limit(ServoID, std::uint8_t tmperature_limit);
+  void set_stretch(ServoID, std::uint8_t stretch);
+  void set_speed(ServoID, std::uint8_t speed);
+  void set_current_limit(ServoID, std::uint8_t current_limit);
+  void set_temperature_limit(ServoID, std::uint8_t tmperature_limit);
 
   std::uint8_t get_stretch(ServoID id);
   std::uint8_t get_speed(ServoID id);
