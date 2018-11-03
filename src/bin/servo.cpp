62386e35e706ee977c017b4509f2070e8150e1a6
coord.e <me@coord-e.com>
2018-11-03 14:47:48 +0900

    Fix: FIx typo

--- src/bin/servo.cpp
+++ src/bin/servo.cpp
@@ -11,10 +11,10 @@ int main(int argc, char **argv) {
 
   const std::string device {argv[1]};
   const std::uint8_t en_pin  = static_cast<std::uint8_t>(strtol(argv[2], nullptr, 0));
-  const IcsServo::ServoID id  = static_cast<IcsServo::ServoID>(strtol(argv[3], nullptr, 0));
+  const ICSServo::ServoID id  = static_cast<ICSServo::ServoID>(strtol(argv[3], nullptr, 0));
 
-  auto sa = std::make_shared<IcsServo::IOProvider>(device, B115200, en_pin);
-  auto servo = IcsServo::Servo(sa, id);
+  auto sa = std::make_shared<ICSServo::IOProvider>(device, B115200, en_pin);
+  auto servo = ICSServo::Servo(sa, id);
 
   while(true) {
     double rad;
