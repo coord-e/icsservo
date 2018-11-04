8059a3b8a90e088956c254fecc6ed23c60aa57fe
coord.e <me@coord-e.com>
2018-11-05 03:52:25 +0900

    Add: Add a discrete binding to ICSServo::Servo

--- src/python/binding.cpp
+++ src/python/binding.cpp
@@ -26,6 +26,19 @@ public:
 
 PYBIND11_MODULE(servoarray, m) {
   m.doc() = "ICSServo: ICS serial servo driver library";
+  py::class_<::ICSServo::Servo>(m, "Servo")
+    .def("set_position", &::ICSServo::Servo::set_position)
+    .def("set_free", &::ICSServo::Servo::set_free)
+    .def("set_stretch", &::ICSServo::Servo::set_stretch)
+    .def("set_speed", &::ICSServo::Servo::set_speed)
+    .def("set_current_limit", &::ICSServo::Servo::set_current_limit)
+    .def("set_temperature_limit", &::ICSServo::Servo::set_temperature_limit)
+    .def("get_stretch", &::ICSServo::Servo::get_stretch)
+    .def("get_speed", &::ICSServo::Servo::get_speed)
+    .def("get_current", &::ICSServo::Servo::get_current)
+    .def("get_temperature", &::ICSServo::Servo::get_temperature)
+    .def("get_position", &::ICSServo::Servo::get_position);
+
   py::class_<Adaptor::IOProvider>(m, "IOProvider")
     .def(py::init<std::string, std::size_t>(), py::arg("device"), py::arg("en_idx"))
     .def("set_id", &Adaptor::IOProvider::set_id)
