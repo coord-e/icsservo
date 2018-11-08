9ce49848c6350e9789a1e4587f2903d16a5a1345
coord.e <me@coord-e.com>
2018-11-08 07:49:19 +0000

    Fix: Exit gracefully on SIGINT

--- src/bin/servo.cpp
+++ src/bin/servo.cpp
@@ -1,6 +1,18 @@
 #include "ics-servo/servo.h"
 
 #include <iostream>
+#include <signal.h>
+#include <unistd.h>
+#include <cstring>
+#include <atomic>
+
+std::atomic<bool> quit(false);    // signal flag
+
+void on_signal(int);
+void on_signal(int)
+{
+  quit.store(true);
+}
 
 int main(int argc, char **argv) {
 
@@ -13,8 +25,14 @@ int main(int argc, char **argv) {
   const std::uint8_t en_pin  = static_cast<std::uint8_t>(strtol(argv[2], nullptr, 0));
   const ICSServo::ServoID id  = static_cast<ICSServo::ServoID>(strtol(argv[3], nullptr, 0));
 
-  auto sa = std::make_shared<ICSServo::IOProvider>(device, en_pin);
-  auto servo = ICSServo::Servo(sa, id);
+  auto io = std::make_shared<ICSServo::IOProvider>(device, en_pin);
+  auto servo = ICSServo::Servo(io, id);
+
+  struct sigaction sa;
+  memset( &sa, 0, sizeof(sa) );
+  sa.sa_handler = on_signal;
+  sigfillset(&sa.sa_mask);
+  sigaction(SIGINT, &sa, NULL);
 
   while(true) {
     double rad;
@@ -22,5 +40,7 @@ int main(int argc, char **argv) {
     std::cin >> rad;
 
     servo.set_position(rad);
+
+    if(quit.load()) break;
   }
 }
